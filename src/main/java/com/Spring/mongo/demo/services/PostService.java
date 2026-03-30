95ce5eabf8fbbf4e85672e622a1983514f7acdbb
Sávio Romário <savioromario10@gmail.com>
2026-03-30 17:16:38 -0300

    Obtendo postagem por id

---
+++ src/main/java/com/Spring/mongo/demo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.Spring.mongo.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Spring.mongo.demo.domain.Post;  
+import com.Spring.mongo.demo.repositories.PostRepository;
+import com.Spring.mongo.demo.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository repository;
+
+  public Post findById(String id){
+    Post post = repository.findOne(id);
+    if(post == null){
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    }
+    return post;
+  }
+}
